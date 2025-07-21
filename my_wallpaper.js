dde12c5809e79076d72c27b21a99cba661f5b814
aWake3253 <adamwake28@gmail.com>
2025-07-22 10:40:14 +1200

    Update my_wallpaper.js
    working on integrating a for loop, creating a star like circle.

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,11 +1,12 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 30;
-
+let tri_point = 30;
+let x  = 100;
+let y = 100;
+let size = 80;
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+ // pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
@@ -13,13 +14,25 @@ function setup_wallpaper(pWallpaper) {
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 200;
 }
 
 function wallpaper_background() {
   background(240, 255, 240); //light honeydew green colour
 }
 
-function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+function my_symbol() {// do not rename this function. Treat this similarly to a Draw function
+  
+  for (let i = 0; i<tri_point; i++){
+    let angle = (PI/tri_point*120*(i));
+    let tx = x +size *cos (angle);
+    let ty = y +size *sin (angle);
+
+    push();
+    translate(tx, ty);
+    rotate(angle);
+    triangle(0, -15, -15, 10, 10, 10);
+
+  pop();
+}
 }
