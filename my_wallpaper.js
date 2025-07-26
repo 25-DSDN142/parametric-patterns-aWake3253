ff510e6bb7ce69fd3dee86f25cfdd0f37cb65456
aWake3253 <adamwake28@gmail.com>
2025-07-26 12:06:44 +1200

    added randomColor functionality

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,14 +2,27 @@
 let tri_point = 30;
 let x  = 100;
 let y = 100;
-let size = 80;
+let size = 100;
+function drawTriangleAt(tx, ty, angle) { 
+  push();
+  translate(tx, ty);
+  rotate(angle*random());
+  triangle(0, -15, -15, 10, 10, 10);
+  pop();
+}
+
+function randomColor(){
+  let r = random (255);
+  let g = random (255);
+  let b = random (255);
+  return color (r, g, b);
+}
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
- // pWallpaper.output_mode(GRID_WALLPAPER);
-  
+ pWallpaper.output_mode(DEVELOP_GLYPH);
+//pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -22,17 +35,18 @@ function wallpaper_background() {
 }
 
 function my_symbol() {// do not rename this function. Treat this similarly to a Draw function
-  
-  for (let i = 0; i<tri_point; i++){
-    let angle = (PI/tri_point*120*(i));
-    let tx = x +size *cos (angle);
-    let ty = y +size *sin (angle);
+  let angleStep = (PI / tri_point) * 120;
+ 
+ if (tri_point >0){
+    for (let i = 0; i<tri_point; i++){
+      let angle = (angleStep*i);
+      let tx = x +size *cos (angle);
+      let ty = y +size *sin (angle);
+      randomColor();
+      drawTriangleAt(tx, ty, angle);
 
-    push();
-    translate(tx, ty);
-    rotate(angle);
-    triangle(0, -15, -15, 10, 10, 10);
-
-  pop();
+ }
 }
+ 
+ 
 }
