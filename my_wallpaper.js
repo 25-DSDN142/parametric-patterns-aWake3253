6b2a1fb6e83310ba87667b26810dc102d828bf98
aWake3253 <adamwake28@gmail.com>
2025-08-03 20:08:10 +1200

    finalised system, started uploading settings docs

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,61 +1,63 @@
-let tri_point = 30; //allows user to change the amount of triangles in a layer
-let size = 40; // allows user to change the spacing of the layers
-let layers = 3; //allows user to change amount of concentric circle layers
-let strokeThickness = 10; //allows user to change thickness of
-let triangleScale = 3; //allows user to change size of triangle
+let tri_point = 100; //allows user to change the amount of triangles in a layer
+let size = 30; // allows user to change the spacing of the layers
+let layers = 4; //allows user to change amount of concentric circle layers
+let strokeThickness = 0; //allows user to change thickness of rings
+let triangleScale = 4; //allows user to change size of triangle
+
 function randomColorPalette(){ //allows user to edit/add colours to the color palette
 let colorPalette = [
-color(255, 150, 50, 180),
-color(215, 170, 50, 180),
-color(255, 127, 50, 180),
-color(255, 178, 37, 180),
+color(245, 187, 0, 180),
+color(236, 159, 5, 180),
+color(191, 49, 0, 180),
 ];
 return colorPalette[int(random(colorPalette.length))];
-
 }
+
+function wallpaper_background() {
+  background(234, 180, 100); //changes background color
+}
+// set line 58 to orange
+// set line 52 to green
+
 function drawTriangleAt(tx, ty, angle, s) {  //draws the triangle 
   push();
   translate(tx, ty);
   rotate(angle);
-  if (layers>3){
-    triangleScale = 1
-  } else {scale(s)};
+  scale(s);
   triangle(-3, -10, -10, 5, 5, 5);
   pop();
 }
 function setup_wallpaper(pWallpaper) {
  //pWallpaper.output_mode(DEVELOP_GLYPH);
  pWallpaper.output_mode(GRID_WALLPAPER);
- pWallpaper.resolution(FIT_TO_SCREEN);
+ pWallpaper.resolution(NINE_PORTRAIT);
  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
  //Grid settings
  pWallpaper.grid_settings.cell_width  = 200;
  pWallpaper.grid_settings.cell_height = 200;
- pWallpaper.grid_settings.row_offset  = 200;
+ pWallpaper.grid_settings.row_offset  = 300;
 }
-function wallpaper_background() {
-  background(100, 100, 100); //light honeydew green colour
-}
+
 function my_symbol(x, y) { // sets up the tile layout
  if (tri_point >0){
-  for (let layer = 0; layer<layers; layer ++){ //sets up the ring/concentric circle structure
+  for (let layer = 0; layer<layers; layer ++){ 
     let radius = size*(layer + 1);
-      noFill();
-      stroke(200, 200, 200, 200);
-      strokeWeight(strokeThickness);
-      ellipse(x, y, radius*2, radius*2);
     for (let i = 0; i<tri_point; i++){//draws the triangle points around the ring
      let angle = (PI/tri_point*120*(i));
      let tx = x + radius *cos (angle);
      let ty = y + radius *sin (angle);
      noStroke();
      if (i % 3 === 0){ // makes every third triangle a specified color
-      fill(25, 25, 25, 250)
-     } else {fill(randomColorPalette());}
-     
+      fill(142, 166, 4, 180)
+     }else {fill(randomColorPalette());}
+     let currentScale = (layers >3) ? 1 : triangleScale;
      drawTriangleAt(tx, ty, angle , triangleScale);
      }
+      noFill();
+      stroke(255, 78, 0, 250); //sets up the ring/concentric circle structure
+      strokeWeight(strokeThickness);
+      ellipse(x, y, radius*2, radius*2);
     }
   }
 }
